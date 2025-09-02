c582fdba5b7baf3e3602edc73c155e634c6be453
Z2XYU <127650018+Z2XYU@users.noreply.github.com>
2025-09-02 19:23:59 +0800

    UART2配置DMA

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "dma.h"
 #include "dma2d.h"
 #include "ltdc.h"
 #include "usart.h"
@@ -92,10 +93,12 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART1_UART_Init();
   MX_FMC_Init();
   MX_DMA2D_Init();
   MX_LTDC_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
